0fd1e63e57f39fe46fe39739199fd8bc3609678e
TommasoBertocchiDeveloper <tommasobertocchideveloper04@gmail.com>
2024-03-23 18:49:33 +0100

    *

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const { WebcastPushConnection } = require('tiktok-live-connector');
 
-// Username of someone who is currently live
 let tiktokUsername = "magodomin";
-
-// Create a new wrapper object and pass the username
 let tiktokLiveConnection = new WebcastPushConnection(tiktokUsername);
 
-// Connect to the chat (await can be used as well)
 tiktokLiveConnection.connect().then(state => {
     console.info(`Connected to roomId ${state.roomId}`);
+    tiktokLiveConnection.on('gift', (gift) => {
+        if (gift.giftId === 5655) { 
+            console.log('rose received');
+        }
+    });
 }).catch(err => {
     console.error('Failed to connect', err);
-})
+});
